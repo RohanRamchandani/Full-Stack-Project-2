568cef5291cb02f0a02c670b5d11c0c710727f6d
rohan <rohan@mac.wifi.int.ryerson.ca>
2025-05-17 18:29:54 -0400

    First Commit of the Project

---
+++ backend/models/product.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'; 
+
+const productSchema = new mongoose.Schema({
+    // Product Name
+    name: { 
+        type: String, 
+        required: true
+    }, 
+    price: { 
+        type: Number, 
+        required: true
+    }, 
+    image: { 
+        // Image should be in string as you will be putting in the URL of the image later on
+        type: String, 
+        required: true
+    }, 
+}, {
+    timestamps: true // CreatedAt and UpdatedAt a certain time 
+});
+
+export const Product = mongoose.model('Product', productSchema); 
